a9b90fb2df79e95b2a964c14345892d94057820d
omar-hossam <omarhossam50@proton.me>
2026-07-13 11:25:07 +0300

    feat: init Preact+TS, Tailwind CSS & DaisyUI

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'preact/hooks'
+
+export default function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 text-white font-bold p-3">Hello World!</h1>
+      <button className="btn">CLICK ME</button>
+    </>
+  )
+}
